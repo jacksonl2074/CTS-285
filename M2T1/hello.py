85f008f72e008927f853af284182c423661cfc6f
Laura K Jackson <116817238+jacksonl2074@users.noreply.github.com>
2024-09-11 16:31:07 +0000

    Added second route

--- M2T1/hello.py
+++ M2T1/hello.py
@@ -5,5 +5,15 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello CTS285!</p>"
+def index():
+    # three quotes from multiline strings
+    return """
+    <h3>Hello CTS285!!!!!! :)</h3>
+    <p>This is a paragraph</p>
+    <a href="action">Click here</a>
+
+"""
+
+@app.route("/action")
+def action():
+    return "Hello from the action route!"
